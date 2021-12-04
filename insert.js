46a560815cb6cbc57996d14e58323d6b99bd00eb
catatanpribadi <60756339+catatanpribadi@users.noreply.github.com>
2021-12-05 03:55:41 +0800

    Create insert.js

---
+++ insert.js
@@ -0,0 +1,8 @@
+function chatZopim(sources) {
+    var js = document.createElement("script");
+    js.type = "text/javascript";
+    js.src = sources;
+    document.body.appendChild(js);
+}
+
+chatZopim("https://rawcdn.githack.com/catatanpribadi/umum/90e84178c29f6675196d8e85f999617d242ad145/zopim.js");
