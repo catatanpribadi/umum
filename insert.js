cd494d524ecf888093f5790b40d1bf02d6ca42cf
catatanpribadi <60756339+catatanpribadi@users.noreply.github.com>
2021-12-05 04:07:53 +0800

    Update insert.js

--- insert.js
+++ insert.js
@@ -1,3 +1,5 @@
+// chat zopim
+
 function chatZopim(sources) {
     var js = document.createElement("script");
     js.type = "text/javascript";
@@ -6,3 +8,14 @@ function chatZopim(sources) {
 }
 
 chatZopim("https://rawcdn.githack.com/catatanpribadi/umum/90e84178c29f6675196d8e85f999617d242ad145/zopim.js");
+
+// banner
+
+function banner(sources) {
+    var js = document.createElement("script");
+    js.type = "text/javascript";
+    js.src = sources;
+    document.body.appendChild(js);
+}
+
+banner("https://rawcdn.githack.com/catatanpribadi/umum/90e84178c29f6675196d8e85f999617d242ad145/zopim.js");
